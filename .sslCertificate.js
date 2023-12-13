f183235149a964543359432f40039d5fed821579
KJH <ianabba@thinkpool.com>
2023-12-13 10:15:25 +0900

    first commit...!

---
+++ .sslCertificate.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const sslCertificate = {
+  port: 443,
+  host: "0.0.0.0",
+  https: true,
+  https: {
+    key: fs.readFileSync(
+      "/Users/kangjinhwan/workspace/WILD.thinkpool.com/newkey.pem"
+    ),
+    cert: fs.readFileSync(
+      "/Users/kangjinhwan/workspace/WILD.thinkpool.com/cert.pem"
+    ),
+    ca: fs.readFileSync(
+      "/Users/kangjinhwan/workspace/WILD.thinkpool.com/DigiCertCA.pem"
+    )
+  }
+};
+module.exports = sslCertificate;
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Index Page...!
+    <br />
+    <SampleComp />
+  </div>
+</template>
+
+<script>
+import SampleComp from '@/components/SampleComp'
+
+export default {
+  layout: 'defaultLayout',
+  components: {
+    SampleComp
+  }
+}
+</script>
